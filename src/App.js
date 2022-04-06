7d16fb80817dfe276561edc8ee582dacd42441c3
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-04-06 18:00:10 -0400

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Directory from './components/DirectoryComponent';
 
 class App extends Component {
     render() {
@@ -10,7 +11,9 @@ class App extends Component {
                 <div className="container">
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
+                
                 </Navbar>
+                <Directory />
             </div>
         );
     }
