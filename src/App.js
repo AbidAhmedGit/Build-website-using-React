09c1ae43a2bb0709b101eee36276962154c7a9d9
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-04-07 02:34:06 -0400

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Directory from './components/DirectoryComponent';
+import { CAMPSITES } from './shared/campsites';
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
     render() {
         return (
             <div className="App">
@@ -11,9 +19,9 @@ class App extends Component {
                 <div className="container">
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
-                
+
                 </Navbar>
-                <Directory />
+                <Directory campsites={this.state.campsites} />
             </div>
         );
     }
