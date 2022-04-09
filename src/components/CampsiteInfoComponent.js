9adc9e527abb4bb05148575dfa456c898d287ec8
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-04-09 13:30:24 -0400

    added necessary comments for easier reference of the code

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,8 +4,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 
 class CampsiteInfo extends Component {
 
-
-
+    // Task 2: adding a card component to the CampsiteInfo component view to display more information
     renderCampsite(campsite) {
         return (
             <div className="col-md-5 m-1">
@@ -20,14 +19,21 @@ class CampsiteInfo extends Component {
             )
     }
 
+    // Implement a method inside the CampsiteInfo component named renderComments()
+    // that takes the comments array stored in the campsite object as a parameter
+    // -- i.e. renderComments(comments).
     renderComments(comments) {
         if (comments){
             return(<div className="col-md-5 m-1">
                         <h4>Comments</h4>
+                        {/* Use the array method map on the comments array.
+                        You do not need to set up a variable to store the new array.
+                        See the example in the Code Challenge from this week on constructing lists. */}
                         {comments.map
                             (comment => {
                                 return (<div key={comment.id}>
                                             <p>{comment.text}</p>
+                                            {/* the first line should show the comment text, and the second line should show the comment author and date.  */}
                                             <p>  -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>
                                         </div>)
                             })}
@@ -40,9 +46,13 @@ class CampsiteInfo extends Component {
 
 
     render() {
+        // Inside its render method, check if an object with the name "campsite" (passed in via props)
+        // can be evaluated as truthy (e.g. is not null, is not undefined)
         if (this.props.campsite){
             return (
                 <div className="row">
+                    {/* call the renderCampsite method and pass the campsite to it.
+                    Remember to use this where appropriate. There will be two places you need to use this for this step. */}
                     {this.renderCampsite(this.props.campsite)}
                     {this.renderComments(this.props.campsite.comments)}
                 </div>
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -3,18 +3,27 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import CampsiteInfo from './CampsiteInfoComponent';
 
 class Directory extends Component {
+    // When a constructor is used, then it must take props as an argument.
+    // Props is short for properties and is an important keyword and concept in React
     constructor(props) {
+        // The first line of the constructor must be super(props);
+        // so that the props are communicated to the base constructor in the parent component.
         super(props);
+        // Then, if using state, this.state must be set equal to an object.
+        // It is currently an empty object. We will fill it in next.
         this.state = {
             selectedCampsite: null
         };
     }
 
+    // clicking a card selects a campsite
     onCampsiteSelect(campsite) {
         this.setState({selectedCampsite: campsite});
     }
 
     render() {
+        // This will pull the data for each campsite, using map, from the array you just added to the state.
+        // Then it will go through each to format its name and description and render them all.
         const directory = this.props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
@@ -33,12 +42,8 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                {/* <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div> */}
-                {/* <CampsiteInfo className="campsite" >{this.state.selectedCampsite}</CampsiteInfo> */}
+                {/* Pass the this.state.selectedCampsite object
+                as props to this CampsiteInfo component, using the attribute name of "campsite".  */}
                 <CampsiteInfo campsite={this.state.selectedCampsite} />
 
             </div>
