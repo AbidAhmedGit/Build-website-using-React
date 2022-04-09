2ab996c50f9147aa653b83a9891856ba2d57c6b7
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-04-09 13:10:09 -0400

    task 3 completed

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -20,20 +20,36 @@ class CampsiteInfo extends Component {
             )
     }
 
+    renderComments(comments) {
+        if (comments){
+            return(<div className="col-md-5 m-1">
+                        <h4>Comments</h4>
+                        {comments.map
+                            (comment => {
+                                return (<div key={comment.id}>
+                                            <p>{comment.text}</p>
+                                            <p>  -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>
+                                        </div>)
+                            })}
+                    </div>)
+        }
+        return <div />
+    }
+
+
+
+
     render() {
         if (this.props.campsite){
             return (
                 <div className="row">
                     {this.renderCampsite(this.props.campsite)}
-
+                    {this.renderComments(this.props.campsite.comments)}
                 </div>
             );
             }
-        return (
-            <div>
+        return <div />
 
-            </div>
-            )
     }
 }
 
