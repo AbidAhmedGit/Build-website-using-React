a1717e498a223ac49ad5d246593093b04a8d81d7
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-04-15 23:44:18 -0400

    added breadcrumbs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 function RenderCampsite({campsite}) {
@@ -9,11 +10,13 @@ function RenderCampsite({campsite}) {
         return (
             <div className="col-md-5 m-1">
                 <Card>
+
                     <CardImg top src={campsite.image} alt={campsite.name} />
+
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
+
                 </Card>
             </div>
             )
@@ -53,6 +56,16 @@ function CampsiteInfo(props) {
         if (props.campsite) {
             return (
                 <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments comments={props.comments} />
